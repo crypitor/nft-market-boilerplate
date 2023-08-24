402818c1f32c64f2b13b5d13637b3c1161de011a
cauta <trongcauhcmus@gmail.com>
2023-08-24 13:43:25 +0700

    update upgrade contract

--- migrations/20_upgrade-contract.js
+++ migrations/20_upgrade-contract.js
@@ -1,30 +1,21 @@
 const MetaverseMarket = artifacts.require("MetaverseMarket");
+const ProxyAdmin = artifacts.require('ProxyAdmin');
 const TransparentUpgradeableProxy = artifacts.require('TransparentUpgradeableProxy');
 
 module.exports = async function (deployer, network, accounts) {
     // New market
-    // deployer.deploy(MetaverseMarket, { from: accounts[0], overwrite: true })
-    //     .then(async (instance) => {
-    //         let market = await MetaverseMarket.at(TransparentUpgradeableProxy.address);
-    //         const logic = instance.address;
-    //         const proxyAdmin = await ProxyAdmin.at(ProxyAdmin.address);
-    //         let tx = await proxyAdmin.upgrade(TransparentUpgradeableProxy.address, logic, { from: accounts[0] });
-    //         console.log("Change implementation at tx: " + tx.tx);
-    //         let impl = await proxyAdmin.getProxyImplementation(TransparentUpgradeableProxy.address);
-    //         console.log("New Impl: " + impl);
-    //         let trade = await market.getOrder(0);
-    //         console.log("get order: " + trade.toString());
-    //     });
-
-    let market = await MetaverseMarket.at(TransparentUpgradeableProxy.address);
-    // const logic = '0xb78f2B579d90582B53CB63D2a16b5DFDe231d7C2';
-    // const proxyAdmin = await ProxyAdmin.at(ProxyAdmin.address);
-    // let tx = await proxyAdmin.upgrade(TransparentUpgradeableProxy.address, logic, { from: accounts[0] });
-    // console.log("Change implementation at tx: " + tx.tx);
-    // let impl = await proxyAdmin.getProxyImplementation(TransparentUpgradeableProxy.address);
-    // console.log("New Impl: " + impl);
-    let setToken = await market.setToken('0xD301fC0F091d87f59Db47e0C7255c967c55CDd96');
-    console.log("set token " + setToken.tx);
+    await deployer.deploy(MetaverseMarket, { from: accounts[0], overwrite: true })
+        .then(async (instance) => {
+            let market = await MetaverseMarket.at(TransparentUpgradeableProxy.address);
+            const logic = instance.address;
+            const proxyAdmin = await ProxyAdmin.at(ProxyAdmin.address);
+            let tx = await proxyAdmin.upgrade(TransparentUpgradeableProxy.address, logic, { from: accounts[0] });
+            console.log("Change implementation at tx: " + tx.tx);
+            let impl = await proxyAdmin.getProxyImplementation(TransparentUpgradeableProxy.address);
+            console.log("New Impl: " + impl);
+            let trade = await market.getOrder(0);
+            console.log("get order: " + trade.toString());
+        });
 };
 
 // truffle run verify ProxyAdmin --network bscTestnet
