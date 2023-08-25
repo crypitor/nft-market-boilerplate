607b4366cd31ca61d329c6875e9933564e93d072
cauta <trongcauhcmus@gmail.com>
2023-08-25 09:01:57 +0700

    update migration

--- migrations/1_deploy_token.js
+++ migrations/1_deploy_token.js
@@ -3,6 +3,8 @@ const MyToken = artifacts.require("MyToken");
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(MyToken, 'MyToken', 'MTK', { from: accounts[0], overwrite: true }).then(async (instance) => {
       await instance.mint(accounts[1], '1000000000000000', {from: accounts[0]});
+      const blockNumber = (await MyToken.web3.eth.getTransaction(instance.transactionHash)).blockNumber;
+      console.log(blockNumber);
   });
 };
 // truffle run verify MyToken --network bscTestnet
