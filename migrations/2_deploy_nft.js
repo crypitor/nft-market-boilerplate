6cda420cecc9ffca715850015d94b7e5e2382f31
cauta <trongcauhcmus@gmail.com>
2023-11-26 14:11:12 +0700

    remove old script

--- migrations/2_deploy_nft.js
+++
@@ -1,7 +0,0 @@
-const NFTCore = artifacts.require("NFTCore");
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(NFTCore, { from: accounts[0], overwrite: true }).then(async (instance) => {
-  });
-
-};
