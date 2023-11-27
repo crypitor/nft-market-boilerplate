202e4cd255991945170aa9540d0c4502764ab6d2
cauta <trongcauhcmus@gmail.com>
2023-11-27 17:26:32 +0700

    add support for thirdweb contract

---
+++ contracts/thirdweb/Marketplace.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@thirdweb-dev/contracts/prebuilts/marketplace-legacy/Marketplace.sol";
---
+++ contracts/thirdweb/NFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@thirdweb-dev/contracts/base/ERC721LazyMint.sol";
+import "@thirdweb-dev/contracts/base/ERC721Drop.sol";
+import "@thirdweb-dev/contracts/base/ERC721Base.sol";
